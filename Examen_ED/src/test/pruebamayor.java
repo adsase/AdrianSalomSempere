7e69800c3060d28119a5d92e0f16fb7bccca1430
AdrianSalom <adriansalomsempere@hotmail.com>
2014-06-05 17:02:32 +0200

    Signed-off-by: AdrianSalom<adriansalomsempere@hotmail.com>

---
+++ Examen_ED/src/test/pruebamayor.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import codigo.Mayor;
+
+public class pruebamayor {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	public void test() {
+		assertequals();
+	}
+
+}
